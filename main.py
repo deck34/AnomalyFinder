9a480e970b609296ab5a33b37eaac159799b31d9
DECK34 <deck344@gmail.com>
2017-02-22 18:34:26 +0300

    update variable names new plot style

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import datetime
 import visualize as vz
 
 
-df1 = pd.read_csv("Building0teacher.csv", ';', nrows = 671)
-del df1['\t']
-df1['Energy'] = df1['Energy'].str.replace(',','.')
-df1['Energy'] = pd.to_numeric(df1['Energy'], errors='coerce')
-df1['DateTime'] = pd.to_datetime(df1['DateTime'],format='%d.%m.%Y %H:%M')
+teacher = pd.read_csv("data\Building0teacher.csv", ';', nrows = 671)
+del teacher['\t']
+teacher['Energy'] = teacher['Energy'].str.replace(',','.')
+teacher['Energy'] = pd.to_numeric(teacher['Energy'], errors='coerce')
+teacher['DateTime'] = pd.to_datetime(teacher['DateTime'],format='%d.%m.%Y %H:%M')
 
-vz.visualize(df1)
+vz.visualize(teacher)
