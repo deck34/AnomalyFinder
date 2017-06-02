6c586083288737bfb0cdbc9e758fb5aa82a45cf8
DECK34 <deck344@gmail.com>
2017-06-02 17:52:40 +0300

    Руссификация интерфейса, основные комментарии к коду

--- function.py
+++ function.py
@@ -5,64 +5,118 @@ import pandas as pd
 import string
 import re
 
-ALPHABET = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-ALPHABET_LEN = len(ALPHABET)-1
+class Patterns:
+    #Класс поиска аномальных паттернов в размеченной выборке
 
-def represent(data):
-    dmin = min(data)
-    dmax = max(data)
-    ddelta = dmax - dmin
-    dstep = ddelta / ALPHABET_LEN
-    # normalize input data and present as symbol string
-    representer = lambda d: \
-        ALPHABET[int(np.round(((d - dmin) / ddelta) * ALPHABET_LEN))]
-    return ''.join(map(representer, data))
+    ALPHABET = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    ALPHABET_LEN = len(ALPHABET) - 1
 
-def add_sym_str(data):
-    data_sym_str = represent(data['Energy'])
-    data_sym_str = list(data_sym_str)
+    def represent(data):
+        """Функция поиска символа соответветствующему числу из входного массива
 
-    data.insert(2, 'Energy_sym', data_sym_str)
-    return data
+            data - массив с данными
 
-def count_(list,str):
-    count = 0
-    for i in list:
-        if i == str:
-            count += 1
-    return count
-def find_pattern(data,length):
-    string = ''.join(data['Energy_sym'])
-    indexes = data[data.is_outlier == True].index.tolist()
-    #d_p = {}
-   # d_v = {}
-    list_patterns = []
-    #data.insert(4, 'Pattern', '')
-    #data.insert(5, 'Percent', 0)
-    for i in indexes:
-        if i-length >= 0:
-            tmp = []
-            for j in range(i-length,i):
-                tmp.append(data['Energy_sym'][j])
-            #d_p[i] = ''.join(tmp)
-            list_patterns.append(''.join(tmp))
-    percents = []
-    for pattern in list_patterns:
-        percents.append(count_(list_patterns,pattern)/len(re.findall(pattern,string))*100)
-        #d_v[i] = count_(list_patterns,pattern)/len(re.findall(pattern,string))*100
+            :return - строка символов, соответствующих значениям входного массива
+        """
 
-    return indexes,list_patterns,percents
+        dmin = min(data)
+        dmax = max(data)
+        ddelta = dmax - dmin
+        dstep = ddelta / Patterns.ALPHABET_LEN
+        # normalize input data and present as symbol string
+        representer = lambda d: \
+            Patterns.ALPHABET[int(np.round(((d - dmin) / ddelta) * Patterns.ALPHABET_LEN))]
+        return ''.join(map(representer, data))
+
+    def add_sym_str(df):
+        """Функция добавляет в DataFrame столбец с символьным предствалением
+
+            df - DataFrame с данными
+
+            :return
+                df - DataFrame с добавленной строкой символьного представления значений
+        """
+
+        data_sym_str = Patterns.represent(df['Energy'])
+        data_sym_str = list(data_sym_str)
+
+        df.insert(2, 'Energy_sym', data_sym_str)
+        return df
+
+    def count_(list,str):
+        """Функция выполняет подсчет количества повторений паттернов в списке
+
+            list    - список паттернов
+            str     - строка с паттерном
+
+            :return
+                count - количество повторений
+        """
+
+        count = 0
+        for i in list:
+            if i == str:
+                count += 1
+        return count
+
+    def find_pattern(data,length):
+        """Функция выполняет поиск аномальных паттернов
+
+            data    - DataFrame с данными
+            length     - Длинна искомого паттерна
+
+            :returns
+                indexes - список индексов в DataFrame выборки
+                list_patterns - список аномальных паттернов
+                percents - - список процентов возникновения
+        """
+
+        string = ''.join(data['Energy_sym'])
+        indexes = data[data.is_outlier == True].index.tolist()
+        list_patterns = []
+        for i in indexes:
+            if i-length >= 0:
+                tmp = []
+                for j in range(i-length,i):
+                    tmp.append(data['Energy_sym'][j])
+                #d_p[i] = ''.join(tmp)
+                list_patterns.append(''.join(tmp))
+        percents = []
+        for pattern in list_patterns:
+            percents.append(Patterns.count_(list_patterns,pattern)/len(re.findall(pattern,string))*100)
+
+        return indexes,list_patterns,percents
 
 class SVM:
+    #Класс обучения и поиска аномальных точек
+
     def prepare_data(df):
+        """Функция для подготовки данных к классификации
+
+            df - DataFrame с данными
+
+            :return
+                data_mas - Массив с данными из сходного DataFrame
+        """
         data_mas = np.zeros((np.shape(df['Energy'])[0], 2))
 
         for i in range(0, np.shape(data_mas)[0]):
-            data_mas[i][0] = i #df['DateTime'][i]
+            data_mas[i][0] = i
             data_mas[i][1] = df['Energy'][i]
+
         return data_mas
 
     def clf(df_train, df_test,outlier_fraction = 0.07):
+        """ Функция классификации
+
+            df_train            - обучающая выборка
+            df_test             - тестовая выборка
+            outlier_fraction    - коэффициент для поиска аномальных точек
+
+            :return
+                df_test - расмеченная выборка на основе df_test
+        """
+
         train_data = SVM.prepare_data(df_train)
         test_data = SVM.prepare_data(df_test)
 
@@ -84,9 +138,22 @@ class SVM:
         for i in test_data[is_inlier == 0, 0]:
             df_test.loc[i,'is_outlier'] = True
 
-        #xx, yy = np.meshgrid(np.linspace(0, 70, 700), np.linspace(0, 50, 500))
-        #Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        #Z = Z.reshape(xx.shape)
-
-
         return df_test
+
+class ForData:
+    #Класс для операций с файлами
+
+    def correct_data(df):
+        """Функция для приведения входного файла к нужному форматуи
+
+            df - DataFrame с данными
+
+            :return
+                df - DataFrame с скорректированными форматами данных
+        """
+
+        del df['\t']
+        df['Energy'] = df['Energy'].str.replace(',','.')
+        df['Energy'] = pd.to_numeric(df['Energy'], errors='coerce')
+        df['DateTime'] = pd.to_datetime(df['DateTime'],format='%d.%m.%Y %H:%M')
+        return df
--- main_gui.py
+++ main_gui.py
@@ -2,20 +2,16 @@ import sys
 import matplotlib
 matplotlib.use("Qt5Agg")
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5 import QtCore
 
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 
 import pandas as pd
-import work_with_data as wd
 import function as f
 import numpy as np
-import string
 
 class MainWindow(QMainWindow):
 
@@ -33,25 +29,29 @@ class MainWindow(QMainWindow):
         self.create_menu()
 
     def create_menu(self):
-        self.file_menu = QMenu('&File', self)
-        self.file_menu.addAction("&Load file", self.on_load_file, QtCore.Qt.CTRL + QtCore.Qt.Key_T)
-        self.file_menu.addAction("&Load teacher file", self.on_load_teacherfile, QtCore.Qt.CTRL + QtCore.Qt.Key_Y)
+        # Создание пунктов меню интерфейса
+
+        self.file_menu = QMenu('&Файл', self)
+        self.file_menu.addAction("&Загрузить тестовую выборку", self.on_load_file, QtCore.Qt.CTRL + QtCore.Qt.Key_T)
+        self.file_menu.addAction("&Загрузить обучающую выборку", self.on_load_teacherfile, QtCore.Qt.CTRL + QtCore.Qt.Key_Y)
         self.file_menu.addSeparator()
-        self.file_menu.addAction("&Save plot", self.on_save_plot, QtCore.Qt.CTRL + QtCore.Qt.Key_P)
-        self.file_menu.addAction("&Save marked", self.on_save_marked, QtCore.Qt.CTRL + QtCore.Qt.Key_S)
+        self.file_menu.addAction("&Сохранить график", self.on_save_plot, QtCore.Qt.CTRL + QtCore.Qt.Key_P)
+        self.file_menu.addAction("&Сохранить размеченную выборку", self.on_save_marked, QtCore.Qt.CTRL + QtCore.Qt.Key_S)
         self.file_menu.addSeparator()
-        self.file_menu.addAction('&Quit', self.on_fileQuit, QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.file_menu.addAction('&Выход', self.on_fileQuit, QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
         self.menuBar().addMenu(self.file_menu)
-        self.help_menu = QMenu('&Help', self)
+        self.help_menu = QMenu('&Помощь', self)
         self.menuBar().addSeparator()
         self.menuBar().addMenu(self.help_menu)
-        self.help_menu.addAction('&About', self.on_about)
+        self.help_menu.addAction('&О программе', self.on_about)
 
     def create_main_window(self):
+        # Создание интерфейса главного окна
+
         self.main_frame = QWidget()
 
-        self.button = QPushButton('Start', self)
-        self.button.setToolTip('Press this button')
+        self.button = QPushButton('Поиск', self)
+        self.button.setToolTip('Выполнится поиск аномалий и аномальных паттернов')
         self.button.clicked.connect(self.on_click)
 
         self.dpi = 100
@@ -59,7 +59,7 @@ class MainWindow(QMainWindow):
 
         self.axes = self.fig.add_subplot(111)
         self.axes.tick_params(labelsize=8)
-        self.axes.set_title('Novelty Detection')
+        self.axes.set_title('Time series')
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(self.main_frame)
         self.toolbar = NavigationToolbar(self.canvas, self.main_frame)
@@ -68,7 +68,7 @@ class MainWindow(QMainWindow):
         self.sp_i.setMinimum(2)
         self.sp_i.setMaximum(10)
 
-        self.lbl_i = QLabel("Pattern length")
+        self.lbl_i = QLabel("Длина паттерна")
 
         hbox = QHBoxLayout()
         vbox_ = QVBoxLayout()
@@ -79,13 +79,9 @@ class MainWindow(QMainWindow):
 
         hbox.addLayout(vbox_)
         hbox.setAlignment(vbox_, Qt.AlignTop)
-        """
-        for w in [self.button]:
-            hbox.addWidget(w)
-            hbox.setAlignment(w, Qt.AlignTop)
-        """
 
         self.tb = QPlainTextEdit()
+        self.tb.setReadOnly(True)
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.canvas)
@@ -94,41 +90,44 @@ class MainWindow(QMainWindow):
         hbox.addWidget(self.tb)
         self.main_frame.setLayout(hbox)
         self.setCentralWidget(self.main_frame)
-        self.statusBar().showMessage('Welcome!', 2000)
+        self.statusBar().showMessage('Добро пожаловать!', 2000)
 
-    def plot(self):
+    def search(self):
+        #В этой функции выполняется обучение, поик аномальных точек и аномальных паттернов во тестовом временном ряду
+
         try:
             outlier_fraction = 0.07
             pattern_length = self.sp_i.value()
-            data_train = f.SVM.clf(self.teacher,self.data,outlier_fraction)
-            #n_inliers = int((1. - outlier_fraction) * np.shape(data_train[data_train.is_outlier == False])[0])
-            #n_outliers = int(outlier_fraction * np.shape(data_train[data_train.is_outlier == True])[0])
+            data_train = f.SVM.clf(self.teacher, self.data, outlier_fraction)
+            # n_inliers = int((1. - outlier_fraction) * np.shape(data_train[data_train.is_outlier == False])[0])
+            # n_outliers = int(outlier_fraction * np.shape(data_train[data_train.is_outlier == True])[0])
 
             # plot the line, the points, and the nearest vectors to the plane
-            #self.axes.contour(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7))
-            #a = self.axes.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
-            #self.axes.contour(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
+            # self.axes.contour(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7))
+            # a = self.axes.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
+            # self.axes.contour(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
 
-            self.data_marked = f.add_sym_str(data_train)
-            indexes, pattern, percents = f.find_pattern(self.data_marked, pattern_length)
+            self.data_marked = f.Patterns.add_sym_str(data_train)
+            indexes, pattern, percents = f.Patterns.find_pattern(self.data_marked, pattern_length)
             self.tb.setPlainText('')
             for i in range(0, len(indexes)):
-                self.tb.appendPlainText('Anomaly in date %s with percent %f\nPattern: \'%s\' \n' % ( self.data_marked['DateTime'][indexes[i]].strftime("%d-%m-%Y %H:%M"), percents[i], pattern[i]))
+                self.tb.appendPlainText('Anomaly in date %s with percent %f\nPattern: \'%s\' \n' % (
+                                        self.data_marked['DateTime'][indexes[i]].strftime("%d-%m-%Y %H:%M"), percents[i], pattern[i]))
 
-           # b1 = self.axes.plot(self.teacher['DateTime'], self.teacher['Energy'], c='black', label = "train")
+                # b1 = self.axes.plot(self.teacher['DateTime'], self.teacher['Energy'], c='black', label = "train")
 
             b2 = self.axes.plot(data_train['DateTime'], data_train['Energy'], color='green', label="test")
             outlier = data_train[data_train.is_outlier == True]
             Xouniques, Xo = np.unique(outlier['DateTime'], return_index=True)
 
             b3 = self.axes.scatter(Xouniques, outlier['Energy'], color='red', s=30)
-            #self.axes.legend([b3],
+            # self.axes.legend([b3],
             #                 ['outliers_test'],
             #                 loc="upper left",
             #                 prop=matplotlib.font_manager.FontProperties(size=11))
             self.axes.set_ylabel('Energy')
             self.axes.set_xlabel('DateTime')
-            #self.axes.set_yticklabels(self.data_marked['Energy_sym'])
+            # self.axes.set_yticklabels(self.data_marked['Energy_sym'])
             self.show()
             self.canvas.draw()
 
@@ -137,27 +136,48 @@ class MainWindow(QMainWindow):
             self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0], 2000)
 
     def on_click(self):
-        self.plot()
+        #Функция вызываемая при нажатии кнопки "Поиск
+
+        all_load = True
+
+        try:
+            self.teacher
+        except  Exception:
+            all_load = False
+            QMessageBox.about(self, "Ошибка",
+                              """Перед поиском необходимо загрузить обучающую выборку.""")
+
+        try:
+            self.data
+        except  Exception:
+            all_load = False
+            QMessageBox.about(self, "Ошибка",
+                              """Перед поиском необходимо загрузить тестовую выборку.""")
+
+        if all_load:
+            self.search()
 
     def on_load_teacherfile(self):
-        print("load teacher")
+        #Функция загружающая выбранный файл обучающей выборки
+
         try:
             file_choices = "CSV (*.csv)|*.csv"
-            path = (QFileDialog.getOpenFileName(self, 'Load teacher file', '',file_choices))
+            path = (QFileDialog.getOpenFileName(self, 'Загрузка обучающей выборки', '',file_choices))
             self.teacher = pd.read_csv(path[0], ';', nrows=67) #671
-            self.teacher = wd.ForData.correct_data(self.teacher)
-            self.statusBar().showMessage('ВЖУХ an file %s has opened' % path[0], 2000)
+            self.teacher = f.ForData.correct_data(self.teacher)
+            self.statusBar().showMessage('Файл %s загружен' % path[0], 2000)
         except  Exception:
             self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0], 2000)
 
     def on_load_file(self):
-        print("load file")
+        # Функция загружающая выбранный файл тестовой выборки
+
         try:
             file_choices = "CSV (*.csv)|*.csv"
-            path = (QFileDialog.getOpenFileName(self, 'Load test file', '',file_choices))
+            path = (QFileDialog.getOpenFileName(self, 'Загрузка тестового файла', '',file_choices))
             self.data = pd.read_csv(path[0], ';', nrows=67)
-            self.data = wd.ForData.correct_data(self.data)
-            self.statusBar().showMessage('VJUH an file %s has opened' % path[0], 2000)
+            self.data = f.ForData.correct_data(self.data)
+            self.statusBar().showMessage('Файл %s открыт' % path[0], 2000)
         except  Exception:
             self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0], 2000)
 
@@ -168,25 +188,37 @@ class MainWindow(QMainWindow):
         self.on_fileQuit()
 
     def on_about(self):
-        QMessageBox.about(self, "About","""This program for search of anomaly patterns in temporary ranks of energy consumption on the basis of the symbolical analysis.""")
+        QMessageBox.about(self, "О программе","""Эта программа выполняет поиск аномальных паттернгов во временном ряду энергопотребления при помощи символьного анализа.""")
 
     def on_save_plot(self):
+        #Функция сохранения графика в файл
+
         file_choices = "PNG (*.png)|*.png"
-        path = (QFileDialog.getSaveFileName(self,'Save file', '',file_choices))
+        path = (QFileDialog.getSaveFileName(self,'Сохранить график', '',file_choices))
         if path:
             self.canvas.print_figure(filename = path[0], format = 'png', dpi=self.dpi)
-            self.statusBar().showMessage('Saved to %s' % path[0], 2000)
+            self.statusBar().showMessage('График сохранен в %s' % path[0], 2000)
 
     def on_save_marked(self):
-        print("save file")
+        #Функция сохранения размеченного временного ряда
+
+        isfile = True
         try:
-            file_choices = "CSV (*.csv)|*.csv"
-            path = (QFileDialog.getSaveFileName(self, 'Save marked file', '',file_choices))
-            if path:
-                self.data_marked.to_csv('marked.csv')
-            self.statusBar().showMessage('VJUH an file %s has saved' % path[0], 2000)
+            self.data_marked
         except  Exception:
-            self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0], 2000)
+            isfile = False
+            QMessageBox.about(self, "Ошибка",
+                              """Перед сохранением необходимо произвести поиск аномальных паттернов.""")
+
+        if isfile:
+            try:
+                file_choices = "CSV (*.csv)|*.csv"
+                path = (QFileDialog.getSaveFileName(self, 'Сохранить размеченный временной ряд', '',file_choices))
+                if path:
+                    self.data_marked.to_csv(path[0])
+                self.statusBar().showMessage('Сохранен в %s' % path[0], 2000)
+            except  Exception:
+                self.statusBar().showMessage('Exception: %s' % sys.exc_info()[0], 2000)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
